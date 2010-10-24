dac35820733918cbe5c3941328a20d08e0097f64
Ray Hernandez <hernan43@gmail.com>
2010-10-24 19:53:21 -0400

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoExchange::Application.config.secret_token = '4a1220d6231fd6ca0968b854fa86707450b31637eb677549a0d74d249909c13062d4a3511cf89afb61fe6b78a178fa4de939fc6596a1491bd21131d18f341904'
